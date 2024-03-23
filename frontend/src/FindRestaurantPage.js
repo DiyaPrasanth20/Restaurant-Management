3a7ce7c83678057095429082242ad7ff283c0ea5
dprasant <dprasant@purdue.edu>
2024-03-22 23:38:34 -0400

    added new page

--- frontend/src/FindRestaurantPage.js
+++ frontend/src/FindRestaurantPage.js
@@ -1,10 +1,10 @@
-// FindRestaurantPage.js
 import React from 'react';
+import './FindRestaurantPage.css'; // Import the CSS file
 
 const FindRestaurantPage = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '20px' }}>
-      <h1>Find Your Favorite Restaurant</h1>
+    <div className="find-restaurant-container"> {/* Use the class name from the CSS file */}
+      <h1>Let's Find Your New Favorite Restaurant!</h1>
       {/* Add more content related to finding restaurants here */}
     </div>
   );
