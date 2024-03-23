d23e32fc8539c61236b1f8ee8c684c3f0ad76851
dprasant <dprasant@purdue.edu>
2024-03-22 21:45:35 -0400

    added styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import imageOne from './images/imageOne.jpg';
+import imageThree from './images/imageThree.jpg';
 
 function App() {
+  const [showReservationPrompt, setShowReservationPrompt] = useState(false);
+
+  const handleAddReservationClick = () => {
+    setShowReservationPrompt(true);
+  };
+
+  const handleYesButtonClick = () => {
+    // Handle Yes button click
+  };
+
+  const handleNoButtonClick = () => {
+    // Handle No button click
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="top-container">
+        <img src={imageOne} alt="Image One" className="image-one" />
+      </div>
+      <div className="middle-container">
+        <h2 className="welcome-text">Welcome to BooknDine!</h2>
+        {!showReservationPrompt && (
+          <div className="buttons">
+            <button className="reservation-button" onClick={handleAddReservationClick}>Add Reservation</button>
+            <button className="reservation-button">Delete/Modify Reservation</button>
+          </div>
+        )}
+        {showReservationPrompt && (
+          <div className="reservation-prompt">
+            <h2>Do you know what you want to eat?</h2>
+            <div className="buttons">
+              <button className="reservation-button" onClick={handleYesButtonClick}>Yes</button>
+              <button className="reservation-button" onClick={handleNoButtonClick}>No</button>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="bottom-container">
+        <img src={imageThree} alt="Image Three" className="image-three" />
+      </div>
     </div>
   );
 }
